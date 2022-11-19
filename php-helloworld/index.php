485a7ef983b5fa64f5caf46e36dca44c9a10ddde
teguhestu <114375545+teguhestu@users.noreply.github.com>
2022-11-19 12:45:21 +0700

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
-print "Hello, ayanggkuu " . PHP_VERSION . "\n";
-print "makasi yaa udaa mau sama sama ama akuu lupp uu cantikknyaa akuu!\n";
+print "Hello, belanda lovers " . PHP_VERSION . "\n";
+print "fokus pildun!\n";
 ?>
